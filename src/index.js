9e0b860c6625eb102c2f09a165696f8d1a9a680a
Pedro <pedrofellipeunai@gmail.com>
2021-02-10 21:11:54 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+const { PubSub } = require('apollo-server')
+const fs = require('fs');
+const path = require('path');
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+
+const { getUserId } = require('./utils');
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Subscription,
+  Link
+}
+
+const prisma = new PrismaClient()
+const pubsub = new PubSub()
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
+});
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
